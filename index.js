8e6560d02d52cb91062dedd314183426e4730c6f
Ruman Ak <ruman7ak@gmail.com>
2023-01-11 10:16:39 +0300

    updated from const to var

--- index.js
+++ index.js
@@ -3,26 +3,22 @@ const cashGiven= document.querySelector("#cash-given");
 const checkbtn= document.querySelector("#check-btn");
 const msg= document.querySelector("#error-msg");
 const numberOfnotes= document.querySelectorAll(".noofnotes");
-const availableNotes= [2000,500,100,20,10,5,1];
+var availableNotes= [2000,500,100,20,10,5,1];
 
 checkbtn.addEventListener("click", function(validateBillAndCashAmt){
     hideMessage();
-    if (billAmt.value > 0){
-        if(billAmt.value <= cashGiven.value ){
+        if(billAmt.value<=cashGiven.value ){
             var AmtToBeReturned= cashGiven.value-billAmt.value;
             calculateChange(AmtToBeReturned);
         }
         else{
             showMessage("Do you wanna wash plates");
         }
-    }else{
-        showMessage("Wrong value entered");
-    }
-})
+    })
 
 function calculateChange(AmtToBeReturned){
     for(i=0;i<availableNotes.length;i++){
-        const noOfNotes= Math.trunc(AmtToBeReturned/availableNotes[i]
+        var noOfNotes= Math.trunc(AmtToBeReturned/availableNotes[i]
         );
         AmtToBeReturned= AmtToBeReturned%availableNotes[i];
         numberOfnotes[i].innerText=noOfNotes;
